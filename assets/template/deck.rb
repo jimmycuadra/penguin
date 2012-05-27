9294dde57768c5bae9f8af1435d6e009ec0fb169
Jimmy Cuadra <jimmy@jimmycuadra.com>
2012-05-26 22:06:51 -0700

    Evaluate config file in Sinatra context. Set template engine explicitly.

--- assets/template/deck.rb
+++ assets/template/deck.rb
@@ -1,5 +1,7 @@
-# Require template language, CSS preprocessor, and JS preprocessor gems here
-
+# Require and set your preferred template engine
 require "haml"
+set :template_engine, :haml
+
+# Require any CSS and JS preprocessors you want to use
 require "sass"
 require "coffee-script"
--- lib/penguin/application.rb
+++ lib/penguin/application.rb
@@ -1,5 +1,4 @@
 require "sinatra/base"
-require File.expand_path("deck", Dir.pwd)
 
 module Penguin
   class Application < Sinatra::Base
@@ -8,6 +7,10 @@ module Penguin
     set :views, root
     set :static, true
 
+    configure do
+      eval File.read(File.expand_path("deck.rb", root))
+    end
+
     use Sprockets do |env|
       env.append_path root
       env.append_path File.expand_path("../../../assets/css", __FILE__)
@@ -15,7 +18,7 @@ module Penguin
     end
 
     get "/" do
-      haml :deck
+      send(settings.template_engine, :deck)
     end
   end
 end
